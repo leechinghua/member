9e5b77f2cd0bbe411c3a8550595fa09aaa94e9ee
leechinghua <leechenghua@gmail.com>
2024-07-08 10:37:39 +0800

    新增customer&welcome router

---
+++ route/customer.js
@@ -0,0 +1,7 @@
+import express, { Router } from "express";
+import moment from "moment-timezone";
+import db from "./../utils/connect-mysql.js"
+
+const router = express.Router();
+
+export default router;
